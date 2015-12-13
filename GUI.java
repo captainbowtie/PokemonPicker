302d4626cd7abdbb3bfaa058703fd7d9e7d28f84
captainbowtie <allencbarr@gmail.com>
2015-12-13 17:01:03 -0600

    Added Party Selection
    Changed so that you remove one Pokemon at a time until there’s only six
    left.

--- GUI.java
+++ GUI.java
@@ -28,58 +28,105 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class GUI extends JFrame{
-	private JButton[] buttons={new JButton("A"), new JButton("B")};
-	private JPanel[] panels=new JPanel[2];
-	private JLabel[] labels=new JLabel[2];
-	private ImageIcon[] pictures=new ImageIcon[2];
+	private JButton[] buttons={new JButton("0"), new JButton("1"),new JButton("2"),new JButton("3"),new JButton("4"),new JButton("5"),new JButton("6")};
+	private JPanel[] panels=new JPanel[7];
+	private JLabel[] labels=new JLabel[7];
+	private ImageIcon[] pictures=new ImageIcon[7];
 	private ArrayList<Integer> currentNumbers = new ArrayList<Integer>();
 	private ArrayList<Integer> nextNumbers = new ArrayList<Integer>();
 	private Random randomNumbers = new Random();
-	private int[] twoIndexes = new int[2];
-	private int[] twoNumbers = new int[2];
+	private int[] sevenIndexes = new int[7];
+	private int[] sevenNumbers = new int[7];
 	public  void startGUI(){
-		setLayout(new GridLayout(2,2));
+		setLayout(new GridLayout(7,2));
 		for(int i=1;i<718;i++){
 			currentNumbers.add(i);
 		}
-		twoIndexes[0]=randomNumbers.nextInt(717);
-		twoNumbers[0]=currentNumbers.get(twoIndexes[0]);
-		currentNumbers.remove(twoIndexes[0]);
-		twoIndexes[1]=randomNumbers.nextInt(716);
-		twoNumbers[1]=currentNumbers.get(twoIndexes[1]);
-		currentNumbers.remove(twoIndexes[1]);
-		buttons[0].addActionListener(new ButtonListener());
-		buttons[1].addActionListener(new ButtonListener());
-		pictures[0]=new ImageIcon(twoNumbers[0]+".png");
-		pictures[1]=new ImageIcon(twoNumbers[1]+".png");
-		labels[0]=new JLabel(pictures[0]);
-		labels[1]=new JLabel(pictures[1]);
-		panels[0]=new JPanel();
-		panels[0].add(labels[0]);
-		panels[1]=new JPanel();
-		panels[1].add(labels[1]);
-		add(panels[0]);
-		add(panels[1]);
-		add(buttons[0]);
-		add(buttons[1]);
+		for(int i=0;i<7;i++){
+			sevenIndexes[i]=randomNumbers.nextInt(717);
+			sevenNumbers[i]=currentNumbers.get(sevenIndexes[i]);
+			currentNumbers.remove(sevenIndexes[i]);
+			buttons[i].addActionListener(new ButtonListener());
+			pictures[i]= new ImageIcon(sevenNumbers[i]+".png");
+			labels[i]= new JLabel(pictures[i]);
+			panels[i]=new JPanel();
+			panels[i].add(labels[i]);
+			add(panels[i]);
+			add(buttons[i]);
+		}
 		pack();
 		setVisible(true);
 	}
-	private void leftButton(){
-		nextNumbers.add(twoNumbers[0]);
-		nextTwo();
+	private void button0(){
+		nextNumbers.add(sevenNumbers[1]);
+		nextNumbers.add(sevenNumbers[2]);
+		nextNumbers.add(sevenNumbers[3]);
+		nextNumbers.add(sevenNumbers[4]);
+		nextNumbers.add(sevenNumbers[5]);
+		nextNumbers.add(sevenNumbers[6]);
+		nextSeven();
 	}
-	private void rightButton(){
-		nextNumbers.add(twoNumbers[1]);
-		nextTwo();
+	private void button1(){
+		nextNumbers.add(sevenNumbers[0]);
+		nextNumbers.add(sevenNumbers[2]);
+		nextNumbers.add(sevenNumbers[3]);
+		nextNumbers.add(sevenNumbers[4]);
+		nextNumbers.add(sevenNumbers[5]);
+		nextNumbers.add(sevenNumbers[6]);
+		nextSeven();
 	}
-	private void nextTwo(){
+	private void button2(){
+		nextNumbers.add(sevenNumbers[1]);
+		nextNumbers.add(sevenNumbers[0]);
+		nextNumbers.add(sevenNumbers[3]);
+		nextNumbers.add(sevenNumbers[4]);
+		nextNumbers.add(sevenNumbers[5]);
+		nextNumbers.add(sevenNumbers[6]);
+		nextSeven();
+	}
+	private void button3(){
+		nextNumbers.add(sevenNumbers[1]);
+		nextNumbers.add(sevenNumbers[2]);
+		nextNumbers.add(sevenNumbers[0]);
+		nextNumbers.add(sevenNumbers[4]);
+		nextNumbers.add(sevenNumbers[5]);
+		nextNumbers.add(sevenNumbers[6]);
+		nextSeven();
+	}
+	private void button4(){
+		nextNumbers.add(sevenNumbers[1]);
+		nextNumbers.add(sevenNumbers[2]);
+		nextNumbers.add(sevenNumbers[3]);
+		nextNumbers.add(sevenNumbers[0]);
+		nextNumbers.add(sevenNumbers[5]);
+		nextNumbers.add(sevenNumbers[6]);
+		nextSeven();
+	}
+	private void button5(){
+		nextNumbers.add(sevenNumbers[1]);
+		nextNumbers.add(sevenNumbers[2]);
+		nextNumbers.add(sevenNumbers[3]);
+		nextNumbers.add(sevenNumbers[4]);
+		nextNumbers.add(sevenNumbers[0]);
+		nextNumbers.add(sevenNumbers[6]);
+		nextSeven();
+	}
+	private void button6(){
+		nextNumbers.add(sevenNumbers[1]);
+		nextNumbers.add(sevenNumbers[2]);
+		nextNumbers.add(sevenNumbers[3]);
+		nextNumbers.add(sevenNumbers[4]);
+		nextNumbers.add(sevenNumbers[5]);
+		nextNumbers.add(sevenNumbers[0]);
+		nextSeven();
+	}
+	private void nextSeven(){
 		setVisible(false);
-		if(currentNumbers.size()<2){
+		if(currentNumbers.size()<7){
 			for(int i=0;i<nextNumbers.size();i++){
 				currentNumbers.add(nextNumbers.get(i));
 			}
-			if(nextNumbers.size()<4){
+			if(currentNumbers.size()<8){
 				String numbersText="0";
 				for(int i=0;i<currentNumbers.size();i++){
 					numbersText=numbersText.concat(" "+currentNumbers.get(i));
@@ -90,20 +137,17 @@ public class GUI extends JFrame{
 				nextNumbers.remove(0);
 			}
 		}
-		
-		panels[0].remove(labels[0]);
-		panels[1].remove(labels[1]);
-		for(int i=0;i<2;i++){
-			twoIndexes[i]=randomNumbers.nextInt(currentNumbers.size());
-			twoNumbers[i]=currentNumbers.get(twoIndexes[i]);
-			currentNumbers.remove(twoIndexes[i]);
-			pictures[i]=new ImageIcon(twoNumbers[i]+".png");
+		for(int i=0;i<7;i++){
+			panels[i].remove(labels[i]);
+			sevenIndexes[i]=randomNumbers.nextInt(currentNumbers.size());
+			sevenNumbers[i]=currentNumbers.get(sevenIndexes[i]);
+			currentNumbers.remove(sevenIndexes[i]);
+			pictures[i]=new ImageIcon(sevenNumbers[i]+".png");
 			labels[i]=new JLabel(pictures[i]);
 			panels[i].add(labels[i]);
 			add(panels[i]);
+			add(buttons[i]);
 		}
-		add(buttons[0]);
-		add(buttons[1]);
 		pack();
 		setVisible(true);
 
@@ -112,12 +156,20 @@ public class GUI extends JFrame{
 	private class ButtonListener implements ActionListener{
 		public void actionPerformed(ActionEvent e) {
 			if(e.getSource()==buttons[0]){
-				leftButton();
+				button0();
 			}else if(e.getSource()==buttons[1]){
-				rightButton();
+				button1();
+			}else if(e.getSource()==buttons[2]){
+				button2();
+			}else if(e.getSource()==buttons[3]){
+				button3();
+			}else if(e.getSource()==buttons[4]){
+				button4();
+			}else if(e.getSource()==buttons[5]){
+				button5();
+			}else if(e.getSource()==buttons[6]){
+				button6();
 			}
-
 		}
-
 	}
 }
